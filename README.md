3aa594665dfc4688e0a70a69102b9309ddb9af5a
Alina Kovtun <46688871+Shockedshodan@users.noreply.github.com>
2023-06-28 13:15:25 +0100

    updated scripts, updated docker-compose

---
+++ README.md
@@ -0,0 +1,22 @@
+#Polygon Dash
+
+Polygon Dash contain two projects in a one repo:
+ - Python backend app
+ - Vue dashboard frontend
+
+
+## Backend
+
+To run backend you have to:
+    1. 
+
+
+- Run script
+or
+- Run 
+
+## Frontend
+
+Run script to deploy:
+``` 
+
---
+++ deploy_frontend.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Open the frontend folder
+cd frontend
+
+# Run npm install
+npm install
+
+# Run npm run build
+npm run build
+
+# Move files to Nginx www folder
+nginx_www_folder="/var/www/html"
+
+# Check if the Nginx www folder exists
+if [ ! -d "$nginx_www_folder" ]; then
+    echo "Nginx www folder '$nginx_www_folder' not found."
+    exit 1
+fi
+
+# Move contents of build folder to Nginx www folder
+echo "Moving files to Nginx www folder..."
+mv build/* "$nginx_www_folder"
+
+# Print success message
+echo "Deployment completed successfully!"
