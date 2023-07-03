598ddec25bf2ed9210a1dae4c639894cb61e8fc5
Alina Kovtun <46688871+Shockedshodan@users.noreply.github.com>
2023-07-03 20:46:25 +0100

    prod stuff

--- backend/src/polydash/definitions.py
+++ backend/src/polydash/definitions.py
@@ -1,8 +1,17 @@
 import os
+from polydash.log import LOGGER
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ALCHEMY_TOKEN_FILE = os.path.join(ROOT_DIR, 'alchemy_token.txt')
 
-DB_FILE = os.path.join(ROOT_DIR, 'blocks.sqlite')
-DB_P2P_FILE = os.path.join(ROOT_DIR, 'watcher.db')
+DB_DIR = os.path.join(ROOT_DIR, 'dbdata')
+if not os.path.exists(DB_DIR):
+    LOGGER.info('creating dbdata directory')
+    os.makedirs(DB_DIR)
+DB_FILE = os.path.join(DB_DIR, 'blocks.sqlite')
+
+DB_P2P_DIR = os.path.join(ROOT_DIR, 'bordb')
+if not os.path.exists(DB_P2P_DIR):
+    os.makedirs(DB_P2P_DIR)
+DB_P2P_FILE = os.path.join(DB_P2P_DIR, 'watcher.db')
