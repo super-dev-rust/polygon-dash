4ef2575c126c8c4abf9f214e45abb88a669e8ba8
Alina Kovtun <46688871+Shockedshodan@users.noreply.github.com>
2023-07-03 20:49:55 +0100

    2hours

--- backend/src/polydash/rating/live_time_heuristic_a.py
+++ backend/src/polydash/rating/live_time_heuristic_a.py
@@ -60,7 +60,7 @@ def process_block(block_number, block_hash, base_fee, block_timestamp, transacti
 @orm.db_session
 def pending_transactions_by_price_and_nonce(block_timestamp, base_fee):
     pending_txs = {}
-    lower_bound = block_timestamp - 86400000
+    lower_bound = block_timestamp - 7200000
     upper_bound = block_timestamp
     query = TransactionFetched.select_by_sql("SELECT * FROM tx_fetched WHERE tx_first_seen > $lower_bound AND tx_first_seen < $upper_bound")
     LOGGER.debug("Querying pending transactions")
