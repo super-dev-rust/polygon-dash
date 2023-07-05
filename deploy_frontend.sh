1a05956967bd8337ccc24ae91df9b2023e036097
Alina Kovtun <46688871+Shockedshodan@users.noreply.github.com>
2023-07-05 19:51:22 +0100

    fix deployment script

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -12,11 +12,16 @@ npm run build
 # Move files to Nginx www folder
 nginx_www_folder="/var/www/html"
 
+
 # Check if the Nginx www folder exists
 if [ ! -d "$nginx_www_folder" ]; then
     echo "Nginx www folder '$nginx_www_folder' not found."
     exit 1
 fi
+
+# Delete contents of Nginx www folder
+echo "Deleting contents of Nginx www folder..."
+rm -rf "$nginx_www_folder"/*
 
 # Move contents of build folder to Nginx www folder
 echo "Moving files to Nginx www folder..."
