d20a1e17291a4c67cca46f43f4100b6f54ce5119
Vladislava Petrusha <kagami.404@gmail.com>
2023-08-18 19:17:07 +0100

    new header

--- frontend/src/components/layout/AppHeader.vue
+++ frontend/src/components/layout/AppHeader.vue
@@ -1,22 +1,89 @@
+<script setup>
+import AppTabsBlockchains from '@/components/shared/AppTabsBlockchains.vue';
+</script>
+
 <template>
   <header class="guardian-header">
-    <IconGuardian />
+    <nav>
+      <router-link to="/" class="guardian-header__home-link">
+        Guardian Labs
+      </router-link>
+      <div class="guardian-header__central-navigation">
+        <router-link to="/" class="guardian-header__link">
+          About RPC
+        </router-link>
+        <router-link to="/" class="guardian-header__link">
+          Set up Router
+        </router-link>
+        <router-link to="/" class="guardian-header__link">
+          FAQ
+        </router-link>
+      </div>
+      <AppTabsBlockchains class="guardian-header__switch"/>
+    </nav>
   </header>
 </template>
 
-<script>
-import IconGuardian from "@/assets/icons/icon-guardian.svg";
-
-export default {
-  name: "AppHeader",
-  components: {
-    IconGuardian,
-  },
-}
-</script>
-
 <style lang="scss">
+@import "@/assets/colors.scss";
+@import "@/assets/breakpoints.scss";
+
 .guardian-header {
-  height: 96px;
+  padding-top: 2rem;
+
+  .guardian-header__central-navigation {
+    display: flex;
+    gap: 1.5rem;
+    justify-content: center;
+  }
+
+  .guardian-header__home-link {
+    display: none;
+  }
+
+  .guardian-header__link {
+    color: $color-text;
+    font-weight: 600;
+    font-size: 1rem;
+    text-decoration: none;
+  }
+
+  .guardian-header__switch {
+    display: none;
+  }
+}
+
+@media (min-width: $breakpoint-tablet) {
+  .guardian-header {
+    padding-top: 3.5rem;
+  }
+}
+
+@media (min-width: $breakpoint-desktop) {
+  .guardian-header {
+    padding-top: 1rem;
+
+    nav {
+      display: flex;
+      gap: 1.5rem;
+      justify-content: space-between;
+      align-items: center;
+      align-content: center;
+    }
+
+    .guardian-header__home-link {
+      display: initial;
+      color: $color-text-heading;
+      text-decoration: none;
+      font-size: 1.25rem;
+      line-height: 1.5rem;
+      font-weight: 600;
+      width: 15rem;
+    }
+
+    .guardian-header__switch {
+      display: flex;
+    }
+  }
 }
 </style>
---
+++ frontend/src/components/shared/AppTabsBlockchains.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref } from 'vue';
+
+import IconMatic from '@/assets/icons/icon-matic.svg';
+import IconAda from '@/assets/icons/icon-ada.svg';
+import IconMaticDisabled from '@/assets/icons/icon-matic-disabled.svg';
+import IconAdaDisabled from '@/assets/icons/icon-ada-disabled.svg';
+
+const blockchains = [
+  {
+    name: 'Polygon',
+    icon: IconMatic,
+    iconDisabled: IconMaticDisabled,
+  },
+  {
+    name: 'Cardano',
+    icon: IconAda,
+    iconDisabled: IconAdaDisabled,
+  },
+];
+
+const activeBlockchain = ref('Polygon');
+const handleBlockchainSelection = (name) => {
+  activeBlockchain.value = name;
+};
+
+</script>
+
+<template>
+<div class="app-tabs">
+  <button
+    v-for="chain in blockchains"
+    class="app-tabs__button"
+    :class="{ 'app-tabs__button--is-active': chain.name === activeBlockchain }"
+    @click="handleBlockchainSelection(chain.name)"
+  >
+    <component :is="chain.name === activeBlockchain ? chain.icon : chain.iconDisabled" />
+    {{ chain.name }}
+  </button>
+</div>
+</template>
+
+<style lang="scss">
+@import "@/assets/colors.scss";
+
+.app-tabs {
+  background: $gray-50;
+  display: flex;
+  gap: 0.5rem;
+  border: 1px solid $gray-200;
+  padding: 0.25rem;
+  border-radius: 0.5rem;
+
+  .app-tabs__button {
+    border: none;
+    border-radius: 6px;
+    background: $gray-50;
+    box-shadow: none;
+    padding: 0.5rem 0.75rem;
+    cursor: pointer;
+    color: $gray-500;
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 1.25rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    &--is-active {
+      background: $base-white;
+      color: $gray-700;
+      box-shadow: 0 1px 2px 0 rgba(16, 24, 40, 0.06), 0 1px 3px 0 rgba(16, 24, 40, 0.10);
+    }
+  }
+}
+</style>
