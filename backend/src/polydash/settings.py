4000698e41139bddee27eb5a937e64a472c62d2d
Alina Kovtun <46688871+Shockedshodan@users.noreply.github.com>
2023-08-19 17:51:18 +0100

    remove hardcoded pass and rpc url - figured out

--- backend/src/polydash/settings.py
+++ backend/src/polydash/settings.py
@@ -7,7 +7,7 @@ class PostgresSettings(BaseSettings):
     # So, the priority is : config > env > default.
     # As the 'user' var is typically set for the current user,
     # it will override the default from this file unless you specify it in the .yaml file.
-    password: str = 'postgres'
+    password: str = None
     user: str = 'postgres'
     host: str = '/tmp/docker'
     port: int = 5432
@@ -15,7 +15,7 @@ class PostgresSettings(BaseSettings):
 
 
 class BlockRetrieverSettings(BaseSettings):
-    block_rpc_url: str = 'https://polygon-mainnet.g.alchemy.com/v2/KcLop5WjVXKCILBnq3JBJwDYyCHHyEdt'
+    block_rpc_url: str = None
 
 
 class PolydashSettings(BaseSettings):
